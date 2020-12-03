02a5f6fb1bd3348cf66f86a5770a9b2efd54d0da
Sam Wong <theesamwong@gmail.com>
2020-12-03 01:38:28 -0800

    added joblistings homework

---
+++ W10D3/job_listing/webpack.config.js
@@ -0,0 +1,29 @@
+var path = require('path');
+var path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: './frontend/entry.jsx',
+  output: {
+    filename: './bundle.js',
+    path: path.resolve(__dirname)
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/env', '@babel/react']
+          }
+        },
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
